cb5c3be3763a2dadb921aaa0b00d01bad96cc633
arohao <119353048+arohao@users.noreply.github.com>
2025-06-20 19:20:09 -0400

    Add bootstrap

--- memory-game/src/App.jsx
+++ memory-game/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './styles/App.css'
+import Nav from './components/Nav'
 
 function App() {
   return (
     <>
+      <Nav/>
       <main>Hi</main>
     </>
   )
--- memory-game/src/components/Nav.jsx
+++ memory-game/src/components/Nav.jsx
@@ -1,7 +1,6 @@
 function Nav() {
   return (
-    <nav>
-      
+    <nav className=''>
     </nav>
   )
 }
