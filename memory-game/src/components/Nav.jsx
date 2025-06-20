778e9cee551670425504a4f0c2d9002562f599c1
arohao <119353048+arohao@users.noreply.github.com>
2025-06-20 19:32:32 -0400

    add logo, add npm i react-router

--- memory-game/src/components/Nav.jsx
+++ memory-game/src/components/Nav.jsx
@@ -1,6 +1,16 @@
+import logo from '../assets/logo.svg'
+
 function Nav() {
+
+
   return (
     <nav className=''>
+      <div>
+        <img alt='test' src={logo} width='50'/>
+        <span className='h3'>Memory Game</span>
+      </div>
+     
+
     </nav>
   )
 }
