13eeb836a4a0570f54451e3ca5294e0c5d537e10
arohao <119353048+arohao@users.noreply.github.com>
2025-06-22 11:03:15 -0400

    Change footer

--- memory-game/src/components/Footer.jsx
+++ memory-game/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import '../styles/App.css';
 function Footer(){
     return(
       <footer className="text-center py-4 bg-light text-muted">
-        &copy; 2025 Created by Wesley Maya and Aroha Upreti
+        &copy; 2025 Memory Game
       </footer>
     );
 }
